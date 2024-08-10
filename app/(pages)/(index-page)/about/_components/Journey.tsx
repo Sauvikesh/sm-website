96751785a46063e2226ed8f1c61897a46621c62d
Sauvikesh <sauviklal@gmail.com>
2024-08-10 13:36:53 -0700

    feat: created my journey section

---
+++ app/(pages)/(index-page)/about/_components/Journey.tsx
@@ -0,0 +1,24 @@
+export default function Journey() {
+  return (
+    <div className="flex flex-col m-40 mt-0 mb-0">
+      <h2 className="text-white font-dm-sans text-4xl w-fit mb-2 font-semibold leading-[54px]">
+        My Journey
+      </h2>
+      <p className="text-white font-dm-sans text-lg w-fit h-fit mb-10 leading-8">
+        Throughout my childhood, I turned my ideas into reality through drawing
+        and building furniture for my dolls. My expansive imagination naturally
+        elected me as the consultant for creative problem solving, from
+        designing all of my relative's birthday cards to choosing the paint
+        color of my dad's motorcycles. ‍
+      </p>
+      <p className="text-white font-dm-sans text-lg w-fit h-fit leading-8">
+        I further fueled my creative endeavors at Design Tech High School where
+        I had the opportunity to thrive in a project-based curriculum modeled
+        after Stanford University's d.school. This sparked a passion for the
+        intersection of human-centered design and technology, and opened up a
+        whole new world where I could continue to approach problems with a
+        creative and empathetic mindset.
+      </p>
+    </div>
+  );
+}
--- app/(pages)/(index-page)/about/page.tsx
+++ app/(pages)/(index-page)/about/page.tsx
@@ -1,9 +1,12 @@
 // import Image from 'next/image';
 
+import Journey from './_components/Journey';
+
 export default function About() {
   return (
-    <main className="">
+    <main className="flex flex-col items-center">
       <div className="text-white">stuff 2</div>
+      <Journey />
     </main>
   );
 }
