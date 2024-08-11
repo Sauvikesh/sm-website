ab2fcb54286dc6ad2cb7f97c4ee81716b6586827
Sauvikesh <sauviklal@gmail.com>
2024-08-10 19:28:01 -0700

    feat: text now dissappears as you scroll through the carousel

--- app/(pages)/(index-page)/about/_components/PhotoCarousel.tsx
+++ app/(pages)/(index-page)/about/_components/PhotoCarousel.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState, useEffect, useRef } from 'react';
+import Image from 'next/image';
 
 const calcDynamicHeight = (objectWidth: number) => {
   const vw = window.innerWidth;
@@ -13,17 +14,37 @@ const handleDynamicHeight = (ref: any, setDynamicHeight: any) => {
   setDynamicHeight(dynamicHeight);
 };
 
-const applyScrollListener = (ref: any, setTranslateX: any) => {
+const applyScrollListener = (
+  ref: any,
+  setTranslateX: any,
+  setTextOpacity: any
+) => {
   window.addEventListener('scroll', () => {
-    const offsetTop = -ref.current.offsetTop + 64; // 64 is the height of the navbar
-    console.log(offsetTop);
-    setTranslateX(offsetTop);
+    if (ref.current) {
+      const offsetTop = -ref.current.offsetTop + 64; // 64 is the height of the navbar
+      setTranslateX(offsetTop);
+
+      // Calculate opacity based on scroll position
+      const start = -50; // Scroll position where opacity starts to change
+      const end = -200; // Scroll position where opacity should be fully changed
+
+      if (offsetTop <= start && offsetTop >= end) {
+        const opacity = 1 - (-1 * (offsetTop - start)) / (-1 * (end - start));
+        console.log('OP1', opacity, offsetTop);
+        setTextOpacity(opacity);
+      } else if (offsetTop > start) {
+        setTextOpacity(1); // Fully visible before start
+      } else if (offsetTop < end) {
+        setTextOpacity(0); // Fully invisible after end
+      }
+    }
   });
 };
 
 export default function PhotoCarousel() {
   const [dynamicHeight, setDynamicHeight] = useState<any>(null);
   const [translateX, setTranslateX] = useState(null);
+  const [textOpacity, setTextOpacity] = useState(1);
 
   const containerRef = useRef(null);
   const objectRef = useRef(null);
@@ -35,8 +56,9 @@ export default function PhotoCarousel() {
   useEffect(() => {
     handleDynamicHeight(objectRef, setDynamicHeight);
     window.addEventListener('resize', resizeHandler);
-    applyScrollListener(containerRef, setTranslateX);
-  }, []);
+    applyScrollListener(containerRef, setTranslateX, setTextOpacity);
+    console.log('x', translateX);
+  }, [translateX]);
 
   return (
     <div className="w-[100%] min-h-[100vh]">
@@ -50,14 +72,47 @@ export default function PhotoCarousel() {
             style={{ transform: `translateX(${translateX}px)` }}
             ref={objectRef}
           >
-            <div className="h-[100%] flex flex-nowrap justify-start items-center ml-8">
+            <div className="h-[100%] flex flex-nowrap justify-start items-center ml-8 gap-4">
+              <div
+                className=" w-[400px] h-[350px] shrink-0 mb-[250px]"
+                style={{
+                  opacity: textOpacity,
+                }}
+              >
+                <h1 className="text-white font-dm-sans text-4xl leading-[54px] font-medium mb-4">
+                  Nice to meet you, I'm Sam
+                </h1>
+                <p className="text-white font-dm-sans text-lg leading-8 font-light">
+                  A visionary designer with an adventurous spirit and an
+                  insatiable curiosity to learn from others
+                </p>
+              </div>
+              <Image
+                src="/samPolaroid.png"
+                alt="Sam polaroid Image"
+                height={1000}
+                width={1000}
+                className="w-auto h-[550px] shrink-0 items-end"
+              ></Image>
+
+              <Image
+                src="/carImg2.jpg"
+                alt="Sam polaroid Image"
+                height={1000}
+                width={1000}
+                className="w-auto h-[550px] shrink-0"
+              ></Image>
+
+              <Image
+                src="/carImg3.jpg"
+                alt="Sam polaroid Image"
+                height={1000}
+                width={1000}
+                className="w-auto h-[550px] shrink-0"
+              ></Image>
               <h1 className="text-white w-[500px] h-[300px] shrink-0 border">
-                hi
+                box here?
               </h1>
-              <h1 className="text-white w-[500px] h-[300px] shrink-0">hi</h1>
-              <h1 className="text-white w-[500px] h-[300px] shrink-0">hi</h1>
-              <h1 className="text-white w-[500px] h-[300px] shrink-0">hi</h1>
-              <h1 className="text-white w-[500px] h-[300px] shrink-0">hi</h1>
             </div>
           </div>
         </div>
