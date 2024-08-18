304b3092e9c8a6e80094e575ac197cca4f9e1c39
Sauvikesh <sauviklal@gmail.com>
2024-08-17 20:51:19 -0700

    improved semantic html and rearranged styles

--- app/(pages)/_components/Navbar/Navbar.tsx
+++ app/(pages)/_components/Navbar/Navbar.tsx
@@ -4,32 +4,34 @@ import AboutLink from './AboutLink';
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center p-8 pt-0 pb-0 h-16">
-      <ul className="">
-        <li className="">
-          <Link href="/">
-            <Image
-              src="/smLogo.png"
-              alt="sm Logo"
-              height={100}
-              width={100}
-              className="h-16 w-auto"
-            ></Image>
-          </Link>
-        </li>
-      </ul>
+    <header>
+      <nav className="flex items-center justify-between h-16 p-8 pt-0 pb-0">
+        <ul>
+          <li>
+            <Link href="/">
+              <Image
+                src="/smLogo.png"
+                alt="sm Logo"
+                height={100}
+                width={100}
+                className="w-auto h-16"
+              ></Image>
+            </Link>
+          </li>
+        </ul>
 
-      <ul className="flex">
-        <AboutLink />
-        <li className="p-5">
-          <Link
-            href="https://drive.google.com/file/d/1JaJ5ppcmlcFHTJpduY-MQ9OY-nYny3V5/view"
-            className="text-white font-dm-sans text-xl leading-none"
-          >
-            Resume
-          </Link>
-        </li>
-      </ul>
-    </nav>
+        <ul className="flex gap-5 p-5">
+          <AboutLink />
+          <li>
+            <Link
+              href="https://drive.google.com/file/d/1JaJ5ppcmlcFHTJpduY-MQ9OY-nYny3V5/view"
+              className="text-xl leading-none text-white font-dm-sans"
+            >
+              Resume
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
