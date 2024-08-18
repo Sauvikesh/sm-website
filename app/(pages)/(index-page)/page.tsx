258d8a4b0e433473eb400dbe4d0c36faea838bc5
Sauvikesh <sauviklal@gmail.com>
2024-08-17 20:54:18 -0700

    moved case study data to an array to pass into component using map function

--- app/(pages)/(index-page)/page.tsx
+++ app/(pages)/(index-page)/page.tsx
@@ -1,28 +1,40 @@
-// import Image from 'next/image';
-import CaseStudy from './_components/CaseStudy';
+import CaseStudy, { CaseStudyProps } from './_components/CaseStudy';
 import Landing from './_components/Landing';
+
 export default function Home() {
+  const caseStudyInformation: CaseStudyProps[] = [
+    {
+      title: 'Paramount+',
+      desc: 'Bolstering live television discovery and modernizing video playback experience.',
+      src: '/caseStudies/paramount.png',
+      alt: 'Paramount Logo',
+    },
+    {
+      title: 'Sage',
+      desc: "Supporting students' mental health journey through personalized self-care practices.",
+      src: '/caseStudies/sage.png',
+      alt: 'Sage App',
+    },
+    {
+      title: 'HackDavis 2024',
+      desc: 'Web application for the largest collegiate hackathon in California. Case study coming soon!',
+      src: '/caseStudies/hackdavis.png',
+      alt: 'HackDavis Website',
+    },
+  ];
+
   return (
-    <main className="">
+    <main>
       <Landing />
-      <CaseStudy
-        title="Paramount+"
-        desc="Bolstering live television discovery and modernizing video playback experience."
-        src="/caseStudies/paramount.png"
-        alt="Paramount Logo"
-      />
-      <CaseStudy
-        title="Sage"
-        desc="Supporting students' mental health journey through personalized self-care practices."
-        src="/caseStudies/sage.png"
-        alt="Sage App"
-      />
-      <CaseStudy
-        title="HackDavis 2024"
-        desc="Web application for the largest collegiate hackathon in California. Case study coming soon!"
-        src="/caseStudies/hackdavis.png"
-        alt="HackDavis Website"
-      />
+      {caseStudyInformation.map((study, index) => (
+        <CaseStudy
+          key={index}
+          title={study.title}
+          desc={study.desc}
+          src={study.src}
+          alt={study.alt}
+        />
+      ))}
     </main>
   );
 }
