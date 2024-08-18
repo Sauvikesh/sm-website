c526dd4cdb1eab4c603d00755f8edecfd583d8b9
Sauvikesh <sauviklal@gmail.com>
2024-08-17 20:55:20 -0700

    multiple changes related to semantic html, spacing, and classes

--- app/(pages)/(index-page)/_components/CaseStudy.tsx
+++ app/(pages)/(index-page)/_components/CaseStudy.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image';
+
 export type CaseStudyProps = {
   title: string;
   desc: string;
   src: string;
   alt: string;
 };
+
 export default function CaseStudy(props: CaseStudyProps) {
   const { title, desc, src, alt } = props;
 
   return (
     <div className="flex flex-col items-center mt-0 justify-normal m-53">
       <h3 className="text-[56px] text-white font-f-w">{title}</h3>
-      <h4 className=" text-[20px] text-white font-dm-sans mb-6">{desc}</h4>
+      <h4 className="text-[20px] text-white font-dm-sans mb-6">{desc}</h4>
       <Image
         src={src}
         alt={alt}
--- app/(pages)/(index-page)/_components/Landing.tsx
+++ app/(pages)/(index-page)/_components/Landing.tsx
@@ -1,6 +1,6 @@
 export default function Landing() {
   return (
-    <div className="flex flex-col ml-[15%] w-[940px] h-[100vh] pt-[8%]">
+    <section className="flex flex-col ml-[15%] w-[940px] h-[100vh] pt-[8%]">
       <h1 className="text-white text-[200px] font-f-w h-[270px] animate-slide-header-down">
         Samantha
       </h1>
@@ -8,12 +8,12 @@ export default function Landing() {
         is a curious product designer who finds joy in human connection and has
         a penchant for creative problem solving.
       </h2>
-      <div className="flex gap-5 mt-36 items-center">
-        <div className="flex border-2 rounded-2xl p-2 h-11 w-8 justify-center">
-          <div className="text-white text-xs animate-slide-line-down">|</div>
+      <div className="flex items-center gap-5 mt-36">
+        <div className="flex justify-center w-8 p-2 border-2 rounded-2xl h-11">
+          <span className="text-xs text-white animate-slide-line-down">|</span>
         </div>
-        <p className="text-white text-sm ">Scroll to explore</p>
+        <p className="text-sm text-white">Scroll to explore</p>
       </div>
-    </div>
+    </section>
   );
 }
