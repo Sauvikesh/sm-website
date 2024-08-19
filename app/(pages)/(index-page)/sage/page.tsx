b3dc0c9fc83622cd468519df88f26be7d01f980c
Sauvikesh <sauviklal@gmail.com>
2024-08-18 21:18:30 -0700

    feat: added scroll parallax library and got it working

---
+++ app/(pages)/(index-page)/sage/page.tsx
@@ -0,0 +1,11 @@
+import Landing from './Landing';
+import { Providers } from '@/app/_providers';
+export default function Sage() {
+  return (
+    <main>
+      <Providers>
+        <Landing />
+      </Providers>
+    </main>
+  );
+}
