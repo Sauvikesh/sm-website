0cb9c727583da4bcb0b67d1da7cc7b9948941429
Sauvikesh <sauviklal@gmail.com>
2024-08-18 18:10:09 -0700

    feat: matched bg and text color to rest of site

--- app/(pages)/(index-page)/login/Form.tsx
+++ app/(pages)/(index-page)/login/Form.tsx
@@ -15,11 +15,13 @@ export default function Form() {
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex flex-col items-center justify-center bg-zinc-400 h-[100vh] gap-4"
+      className="flex flex-col items-center justify-center bg-inherit h-[100vh] gap-4"
     >
-      <h1>please login with password :O</h1>
+      <h1 className="text-white">
+        please login with password to access this page :O
+      </h1>
       <input name="password" type="password" />
-      <button className="border" type="submit">
+      <button className="p-4 text-white border" type="submit">
         Enter
       </button>
     </form>
