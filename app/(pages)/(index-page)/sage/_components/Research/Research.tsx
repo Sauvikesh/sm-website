199c516e118aa791b2d04ab60fbe68c5f2e657b5
Sauvikesh <sauviklal@gmail.com>
2024-08-25 16:04:33 -0700

    feat: added images and text

--- app/(pages)/(index-page)/sage/_components/Research/Research.tsx
+++ app/(pages)/(index-page)/sage/_components/Research/Research.tsx
@@ -1,10 +1,13 @@
 import Header from './Header';
+import Market from './Market';
 import Responses from './Responses';
+
 export default function Research() {
   return (
     <div className="bg-white">
       <Header />
       <Responses />
+      <Market />
     </div>
   );
 }
