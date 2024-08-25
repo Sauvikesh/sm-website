41a346d54468fd238748cfe5007c4b4dc7dc8753
Sauvikesh <sauviklal@gmail.com>
2024-08-25 15:45:26 -0700

    feat: added text and image

--- app/(pages)/(index-page)/sage/_components/Research/Research.tsx
+++ app/(pages)/(index-page)/sage/_components/Research/Research.tsx
@@ -1,8 +1,10 @@
 import Header from './Header';
+import Responses from './Responses';
 export default function Research() {
   return (
     <div className="bg-white">
       <Header />
+      <Responses />
     </div>
   );
 }
---
+++ app/(pages)/(index-page)/sage/_components/Research/Responses.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+export default function Responses() {
+  return (
+    <div className="flex flex-col pt-8 pb-8 p-53 font-dm-sans">
+      <div className="flex pb-28">
+        <h2 className="text-4xl leading-[54px] font-semibold w-[50%]">
+          94 of 98 respondents had difficulty practicing self-care due to lack
+          of time.
+        </h2>
+        <h2 className="text-[100px] leading-[110%] text-blue-600 font-bold w-[50%] text-center">
+          96%
+        </h2>
+      </div>
+      <Image
+        src="/sageImages/research/messageFeedback.jpg"
+        alt="dms image"
+        width={1000}
+        height={1000}
+      />
+    </div>
+  );
+}
