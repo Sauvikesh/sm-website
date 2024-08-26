cb6d5fde6a74e682e67a94d3e1e2b36d0a643f4d
Sauvikesh <sauviklal@gmail.com>
2024-08-26 13:47:50 -0700

    refactor: moved parallax providers to avoid cluttering page.tsx

--- app/(pages)/(index-page)/sage/_components/Landing/Landing.tsx
+++ app/(pages)/(index-page)/sage/_components/Landing/Landing.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Providers } from '@/app/_providers';
 import ParallaxImage, { ParallaxImageProps } from './ParallaxImage';
 
 export default function Landing() {
@@ -22,22 +23,24 @@ export default function Landing() {
   ];
 
   return (
-    <header className="bg-[#e4edeb] flex flex-col justify-center items-center w-full">
-      <h1 className="text-[80px] font-semibold lead-[44px] text-black font-f-w mt-9">
-        Sage
-      </h1>
-      <p className="text-base text-black font-dm-sans">7 min read</p>
+    <Providers>
+      <header className="bg-[#e4edeb] flex flex-col justify-center items-center w-full">
+        <h1 className="text-[80px] font-semibold lead-[44px] text-black font-f-w mt-9">
+          Sage
+        </h1>
+        <p className="text-base text-black font-dm-sans">7 min read</p>
 
-      <div className="flex justify-around w-full mt-32">
-        {parallaxImages.map((image, index) => (
-          <ParallaxImage
-            key={index}
-            speed={image.speed}
-            src={image.src}
-            alt={image.alt}
-          />
-        ))}
-      </div>
-    </header>
+        <div className="flex justify-around w-full mt-32">
+          {parallaxImages.map((image, index) => (
+            <ParallaxImage
+              key={index}
+              speed={image.speed}
+              src={image.src}
+              alt={image.alt}
+            />
+          ))}
+        </div>
+      </header>
+    </Providers>
   );
 }
