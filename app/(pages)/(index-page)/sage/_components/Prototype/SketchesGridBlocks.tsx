afee73f58ccc24e606dbfa30928926c017668759
Sauvikesh <sauviklal@gmail.com>
2024-08-26 15:06:52 -0700

    refactor: modularized grid blocks in sketches section

---
+++ app/(pages)/(index-page)/sage/_components/Prototype/SketchesGridBlocks.tsx
@@ -0,0 +1,15 @@
+export type SketchesGridBlocksProps = {
+  h1: string;
+  p: string;
+};
+
+export default function SketchesGridBlocks(props: SketchesGridBlocksProps) {
+  const { h1, p } = props;
+
+  return (
+    <div className="col-span-1 row-span-1">
+      <h1 className="text-2xl leading-[44px]">{h1}</h1>
+      <p className="text-[18px] leading-8">‍{p}</p>
+    </div>
+  );
+}
