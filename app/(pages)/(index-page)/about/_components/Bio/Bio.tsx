472dce02be9aa7d9718d9d457c350bba276943c5
Sauvikesh <sauviklal@gmail.com>
2024-09-05 23:23:53 -0700

    refactor: used framer motion library to animated slide in text

--- app/(pages)/(index-page)/about/_components/Bio/Bio.tsx
+++ app/(pages)/(index-page)/about/_components/Bio/Bio.tsx
@@ -1,91 +1,46 @@
 'use client';
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 import CarouselImage from '../PhotoCarousel/CarouselImage';
 import CurrentStatus from './CurrentStatus';
+import { motion, useScroll, useTransform } from 'framer-motion';
 
 export default function Bio() {
-  const textStartingTranslation = 15;
+  const sectionRef = useRef(null);
 
-  const [visibilityPercentage, setVisibilityPercentage] = useState(0);
-  const [translateX, setTranslateX] = useState<number>(15);
-  const containerRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ['start 50%', 'end 150%'], // scroll starts 50% above the container, ends by ??% of container(weird math...yucky )
+  });
 
-  useEffect(() => {
-    const copyRef = containerRef;
-    const observer = new IntersectionObserver(
-      (entries) => {
-        const entry = entries[0];
-
-        const transitionStart = 0.3; // start transition when 30% of container is crossed
-        const transitionEnd = 0.7; // end transition by 70% of container being crossed
-
-        if (entry.intersectionRatio < transitionStart) {
-          setTranslateX(textStartingTranslation);
-          setVisibilityPercentage(0);
-        } else if (
-          entry.intersectionRatio >= transitionStart &&
-          entry.intersectionRatio <= transitionEnd
-        ) {
-          const distanceToEnd = transitionEnd - entry.intersectionRatio; // raw distance to end in decimal values
-          const percentProgress =
-            distanceToEnd / (transitionEnd - transitionStart); // calcs percentage distance to end with raw value
-          const visiblePercentProgress = 1 - percentProgress; // since opacitity needs to increase as you scroll, reverse the percent value
-
-          setTranslateX(percentProgress * textStartingTranslation);
-          setVisibilityPercentage(visiblePercentProgress);
-        } else if (entry.intersectionRatio > transitionEnd) {
-          setTranslateX(0);
-          setVisibilityPercentage(1);
-        }
-      },
-      {
-        root: null,
-        rootMargin: '0px',
-        threshold: Array.from({ length: 100 }, (_, i) => i * 0.01),
-      }
-    );
-
-    if (containerRef.current) {
-      observer.observe(containerRef.current);
-    }
-
-    // Cleanup the observer on unmount
-    return () => {
-      if (copyRef.current) {
-        observer.unobserve(copyRef.current);
-      }
-    };
-  }, []);
+  const slideInTextStyle = {
+    x: useTransform(scrollYProgress, [0, 1], [0, 250]),
+    opacity: useTransform(scrollYProgress, [0, 1], [1, 0]),
+  };
 
   return (
-    <section
-      className="flex flex-col m-53 mt-0 mb-0 h-[85vh]"
-      ref={containerRef}
-    >
-      <h1
-        className="text-white font-dm-sans text-[5em] w-max mb-9 font-semibold leading-[54px] self-end duration-75 ease-out"
-        style={{
-          transform: `translateX(${translateX}%)`,
-          opacity: visibilityPercentage,
-        }}
+    <section className="h-[85vh] pl-[15%] pr-[15%]" ref={sectionRef}>
+      <motion.h1
+        className="text-[5em] mb-9 font-semibold leading-[54px]"
+        style={slideInTextStyle}
       >
         Nice to meet you, I'm Sam
-      </h1>
+      </motion.h1>
+
       <article className="flex gap-3">
-        <div className="flex flex-col gap-14 mt-7">
+        <div className="flex flex-col gap-14 pt-7">
           <div>
-            <h2 className="text-4xl font-semibold text-white font-dm-sans">
-              I'm Sam
-            </h2>
-            <p className="text-lg text-white font-dm-sans">
+            <h2 className="text-4xl font-semibold">I'm Sam</h2>
+            <p className="text-lg ">
               A visionary designer with an adventurous spirit and an insatiable
               curiosity to learn from others
             </p>
           </div>
+
           <CurrentStatus
             header="CURRENTLY"
             paragraph="On my way towards earning an undergraduate degree in Design at UC Davis"
           />
+
           <CurrentStatus
             header="PROBABLY"
             paragraph="Drinking a matcha, listening to 70s soul, and solving the daily newspaper Sudoku"
