9261b060de691acd19ba76e38ccbb5f97a2c17a5
Sauvikesh <sauviklal@gmail.com>
2024-09-08 21:43:30 -0700

    fix: navbar now appears on HD and sage pages

--- app/(pages)/_components/Navbar/NavItem.tsx
+++ app/(pages)/_components/Navbar/NavItem.tsx
@@ -6,20 +6,12 @@ interface NavItemProps {
   href: string;
   label: string;
   bgColor: string;
-  navTextColor: string;
 }
 
-export default function NavItem({
-  href,
-  label,
-  bgColor,
-  navTextColor,
-}: NavItemProps) {
+export default function NavItem({ href, label, bgColor }: NavItemProps) {
   return (
     <li className={`${bgColor} p-2 pl-6 pr-6 rounded-[100px] text-[18px]`}>
-      <Link href={href} className={`${navTextColor}`}>
-        {label}
-      </Link>
+      <Link href={href}>{label}</Link>
     </li>
   );
 }
--- app/(pages)/_components/Navbar/Navbar.tsx
+++ app/(pages)/_components/Navbar/Navbar.tsx
@@ -8,11 +8,12 @@ export default function Navbar() {
   const pathname = usePathname();
 
   const bgAboutText = pathname === '/about' ? 'bg-black' : '';
-  const bgWorkText = pathname === '/' ? 'bg-black' : '';
+  const bgWorkText =
+    pathname === '/' || pathname === '/hackdavis' || pathname === '/sage'
+      ? 'bg-black'
+      : '';
   const bgPlayText = pathname === '/play' ? 'bg-black' : '';
-  const navTextColor = pathname === '/sage' ? 'text-black' : 'text-white';
-  const invisible =
-    pathname === '/login' || pathname === '/sage' || pathname === '/hackdavis';
+  const invisible = pathname === '/login';
 
   const isVisible = useDisplayNav();
 
@@ -21,25 +22,22 @@ export default function Navbar() {
       href: '/',
       label: 'Work',
       bgColor: bgWorkText,
-      navTextColor: navTextColor,
     },
     {
       href: 'https://drive.google.com/file/d/1JaJ5ppcmlcFHTJpduY-MQ9OY-nYny3V5/view',
       label: 'Play',
       bgColor: bgPlayText,
-      navTextColor: navTextColor,
     },
     {
       href: '/about',
       label: 'About',
       bgColor: bgAboutText,
-      navTextColor: navTextColor,
     },
   ];
 
   return !invisible ? (
     <header
-      className={`sticky top-0 z-10 flex justify-center bg-transparent pt-12 transition-all duration-300  ${isVisible ? 'opacity-1 translate-y-0' : 'opacity-0 -translate-y-full'}`}
+      className={`fixed top-0 left-[39%] z-50 flex justify-center bg-transparent pt-12 transition-all duration-300  ${isVisible ? 'opacity-1 translate-y-0' : 'opacity-0 -translate-y-full'}`}
     >
       <nav className="flex items-center justify-center bg-[#38373C] w-fit rounded-[100px] font-dm-sans">
         <ul className="flex gap-4 p-2 font-dm-sans text-white text-xl leading-none">
