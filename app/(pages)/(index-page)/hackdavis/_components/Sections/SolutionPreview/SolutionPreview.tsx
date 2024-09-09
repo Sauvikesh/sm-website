966bd4a62af5a98c9012b9c61a77411f2924f906
Sauvikesh <sauviklal@gmail.com>
2024-09-08 20:08:12 -0700

    feat: solution preview buttons now take you to final solution and live site

--- app/(pages)/(index-page)/hackdavis/_components/Sections/SolutionPreview/SolutionPreview.tsx
+++ app/(pages)/(index-page)/hackdavis/_components/Sections/SolutionPreview/SolutionPreview.tsx
@@ -1,5 +1,15 @@
+'use client';
 import Image from 'next/image';
+import Link from 'next/link';
+
 export default function SolutionPreview() {
+  const handleScroll = () => {
+    const finalSolutionSection = document.getElementById('1');
+    if (finalSolutionSection) {
+      finalSolutionSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="flex flex-col pl-[11%] pr-[11%] justify-center items-center">
       <h1 className="pb-4 text-[40px] font-bold tracking-[0.8px]">
@@ -15,12 +25,25 @@ export default function SolutionPreview() {
         participants.
       </p>
       <div className="flex gap-6 pb-20">
-        <button className="bg-[#005271] text-white p-4 rounded-lg">
+        <button
+          className="bg-[#005271] text-white p-4 rounded-lg"
+          onClick={handleScroll}
+        >
           Jump to Solution
         </button>
-        <button className="bg-white text-[#005271] p-4 rounded-lg border border-[#005271]">
-          Experience on the Web
-        </button>
+
+        <Link href="https://2024.hackdavis.io/">
+          <button className="flex items-center gap-2 bg-white text-[#005271] p-4 rounded-lg border border-[#005271]">
+            Experience on the Web
+            <Image
+              src="/hackdavis/linkArrow.svg"
+              alt="link arrow"
+              width={1000}
+              height={1000}
+              className="w-3"
+            />
+          </button>
+        </Link>
       </div>
 
       <div className="flex items-start justify-center gap-5">
