ba57c34121e7c181d8033f175d84a8e6462ac5c4
Sauvikesh <sauviklal@gmail.com>
2024-09-09 23:11:10 -0700

    fix: linter

--- app/(pages)/(index-page)/sage/_components/Summary/Summary.tsx
+++ app/(pages)/(index-page)/sage/_components/Summary/Summary.tsx
@@ -1,46 +1,44 @@
-import GridContentBlock, { GridContentBlockProps } from './GridContentBlock';
-
 export default function Summary() {
-  const gridContent: GridContentBlockProps[] = [
-    {
-      header: 'OVERVIEW',
-      paragraph:
-        "In today's complex world, maintaining good mental health is a fundamental necessity. We designed Sage: the app that advocates for socially accessible mental health and provides personalized self-care practices.",
-      colSpan: 2,
-    },
-    {
-      header: 'TOOLS',
-      paragraph: 'Adobe Photoshop, Illustrator, \nFigma, Qualtrics XM',
-      colSpan: 1,
-    },
-    {
-      header: 'MY ROLE',
-      paragraph:
-        'Led the synthesis of research findings and the identification of pain points, culminating in the ideation of our solutions. Finalized our design system and spearheaded the creation of our hi-fidelity prototype.',
-      colSpan: 2,
-    },
-    {
-      header: 'TIMELINE',
-      paragraph: '6 weeks, Oct - Nov 2022',
-      colSpan: 1,
-    },
-  ];
+  // const gridContent: GridContentBlockProps[] = [
+  //   {
+  //     header: 'OVERVIEW',
+  //     paragraph:
+  //       "In today's complex world, maintaining good mental health is a fundamental necessity. We designed Sage: the app that advocates for socially accessible mental health and provides personalized self-care practices.",
+  //     colSpan: 2,
+  //   },
+  //   {
+  //     header: 'TOOLS',
+  //     paragraph: 'Adobe Photoshop, Illustrator, \nFigma, Qualtrics XM',
+  //     colSpan: 1,
+  //   },
+  //   {
+  //     header: 'MY ROLE',
+  //     paragraph:
+  //       'Led the synthesis of research findings and the identification of pain points, culminating in the ideation of our solutions. Finalized our design system and spearheaded the creation of our hi-fidelity prototype.',
+  //     colSpan: 2,
+  //   },
+  //   {
+  //     header: 'TIMELINE',
+  //     paragraph: '6 weeks, Oct - Nov 2022',
+  //     colSpan: 1,
+  //   },
+  // ];
 
-  const gridContent2: GridContentBlockProps[] = [
-    {
-      header: 'OUTCOMES',
-      paragraph: `⭐️ Awarded "Most Innovative UX" and "Best UX Research" by
-            industry judges from Meta, Amazon & DocuSign`,
-      colSpan: 1,
-    },
-    {
-      header: '__',
-      paragraph: `⭐️ 74% of polled individuals from target audience reported they
-            were either extremely likely or likely to use Sage`,
-      colSpan: 1,
-      transparent: true,
-    },
-  ];
+  // const gridContent2: GridContentBlockProps[] = [
+  //   {
+  //     header: 'OUTCOMES',
+  //     paragraph: `⭐️ Awarded "Most Innovative UX" and "Best UX Research" by
+  //           industry judges from Meta, Amazon & DocuSign`,
+  //     colSpan: 1,
+  //   },
+  //   {
+  //     header: '__',
+  //     paragraph: `⭐️ 74% of polled individuals from target audience reported they
+  //           were either extremely likely or likely to use Sage`,
+  //     colSpan: 1,
+  //     transparent: true,
+  //   },
+  // ];
 
   return (
     <div className="bg-white h-[100vh] flex flex-col justify-around items-center pl-[11%] pr-[11%]">
