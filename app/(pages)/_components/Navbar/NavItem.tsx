f1d5cf580661e5c31e572613b994f87976da4988
Sauvikesh <sauviklal@gmail.com>
2024-09-15 14:38:01 -0700

    feat: navbar now switches to dark mode colors in dark mode

--- app/(pages)/_components/Navbar/NavItem.tsx
+++ app/(pages)/_components/Navbar/NavItem.tsx
@@ -2,6 +2,7 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
+import { useDarkModeSwitch } from '../../_hooks/useDarkModeSwitch';
 
 interface NavItemProps {
   href: string;
@@ -10,11 +11,13 @@ interface NavItemProps {
 }
 
 export default function NavItem({ href, label, bgColor }: NavItemProps) {
+  const darkMode = useDarkModeSwitch();
+
   return (
     <li className={`${bgColor} flex p-2 pl-6 pr-6 rounded-[100px] text-[18px] gap-2`}>
       <Link href={href}>{label}</Link>
       {label === "Resume" &&
-      <Image src='/navLinkArrow.svg' alt='nav link arrow' width={1000} height={1000} className='w-[13px]'/>
+      <Image src='/navLinkArrow.svg' alt='nav link arrow' width={1000} height={1000} className={`w-[13px] ${darkMode ? 'filter invert' : ''}`}/>
       } 
     </li>
   );
--- app/(pages)/_components/Navbar/Navbar.tsx
+++ app/(pages)/_components/Navbar/Navbar.tsx
@@ -3,18 +3,24 @@
 import { usePathname } from 'next/navigation';
 import NavItem from './NavItem';
 import { useDisplayNav } from '../../_hooks/useDisplayNav';
+import { useDarkModeSwitch } from '../../_hooks/useDarkModeSwitch';
 
 export default function Navbar() {
   const pathname = usePathname();
 
-  const bgAboutText = pathname === '/about' ? 'bg-white' : '';
+  const darkMode = useDarkModeSwitch();
+
+  const bgColor = darkMode ? 'bg-black': 'bg-white';
+
+  const bgAboutText = pathname === '/about' ? bgColor : '';
 
   const bgWorkText =
     pathname === '/' || pathname === '/hackdavis' || pathname === '/sage'
-      ? 'bg-white'
+      ? bgColor
       : '';
 
-  const bgPlayText = pathname === '/play' ? 'bg-white' : '';
+  const bgPlayText = pathname === '/play' ? bgColor : '';
+
   const invisible = pathname === '/login';
 
   const isVisible = useDisplayNav();
@@ -41,8 +47,8 @@ export default function Navbar() {
     <header
       className={`fixed top-0 left-[39%] z-50 flex justify-center bg-transparent pt-12 transition-all duration-300  ${isVisible ? 'opacity-1 translate-y-0' : 'opacity-0 -translate-y-full'}`}
     >
-      <nav className="flex items-center justify-center bg-[#F1F1F1] w-fit rounded-[100px] font-dm-sans">
-        <ul className="flex gap-4 p-2 font-dm-sans text-black text-xl leading-none">
+      <nav className={`flex items-center justify-center ${darkMode ? 'bg-[#38373C] text-white': 'bg-[#F1F1F1] text-black'} w-fit rounded-[100px] font-dm-sans`}>
+        <ul className="flex gap-4 p-2 font-dm-sans text-xl leading-none">
           {navItems.map((item, index) => (
             <NavItem key={index} {...item} />
           ))}
