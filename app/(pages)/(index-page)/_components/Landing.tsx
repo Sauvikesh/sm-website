83ee6e998c22c870ba5ed36588680f9374ebf2de
Sauvikesh <sauviklal@gmail.com>
2024-09-15 21:05:59 -0700

    feat: added new landing text with statuses

--- app/(pages)/(index-page)/_components/Landing.tsx
+++ app/(pages)/(index-page)/_components/Landing.tsx
@@ -29,8 +29,23 @@ export default function Landing() {
     },
   ];
 
+  const statuses: StatusesProps[] = [
+    {
+      heading: 'CURRENTLY',
+      text: 'Leading human-centered design education at Davis Design Interactive',
+    },
+    {
+      heading: 'PREVIOUSLY',
+      text: 'Innovating sports and live television streaming experiences at Paramount+',
+    },
+    {
+      heading: 'PROBABLY',
+      text: 'Fueling with matcha, listening to 70s soul, solving the daily newspaper Sudoku',
+    },
+  ];
+
   return (
-    <section className="h-[100vh] pt-[268px]">
+    <section className="h-[100vh] pt-[268px] pl-[8%] pr-[8%]">
       {floatingImages.map((image, index) => (
         <Image
           src={image.src}
@@ -42,23 +57,44 @@ export default function Landing() {
         />
       ))}
 
-      <h1 className="text-[128px] font-f-w h-[200px] animate-slide-header-down w-full text-nowrap">
+      <h1 className="text-[96px] font-f-w h-[140px] font-medium animate-slide-header-down w-full text-nowrap tracking-[3.84px]">
         Samantha Mah
       </h1>
 
       <div className="flex flex-row animate-slide-header-down-delayed">
-        <h2 className="text-4xl w-[90%] font-normal self-start leading-normal tracking-[-0.015em]">
-          is a curious product designer who finds joy in human connection and
-          has a love for creative problem solving
+        <h2 className="text-[25px] font-normal leading-normal tracking-[0.5px]">
+          Designing with empathy, finding joy in creative problem solving,{' '}
+          <br></br> and seeking to understand the intricacies of human beings
         </h2>
+      </div>
+      <div className="flex justify-between pt-16 relative">
+        {statuses.map((status, index) => (
+          <Statuses {...status} key={index} />
+        ))}
         <Image
           src="/landing/matcha.svg"
           width={1000}
           height={1000}
           alt="matcha"
-          className="w-[92px] h-[134px] mt-[-35px]"
+          className="w-[92px] h-[134px] absolute right-[-12%] top-0"
         />
       </div>
     </section>
+  );
+}
+
+type StatusesProps = {
+  heading: string;
+  text: string;
+};
+
+function Statuses({ heading, text }: StatusesProps) {
+  return (
+    <div className="flex flex-col w-[28%]">
+      <h3 className="text-[#A3A3A4] text-[14px] font-normal tracking-[0.56px] font-outfit">
+        {heading}
+      </h3>
+      <p className="text-xs tracking-[0.24px]">{text}</p>
+    </div>
   );
 }
