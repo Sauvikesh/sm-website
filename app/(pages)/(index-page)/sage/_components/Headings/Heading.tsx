18a988b82815daf2cbc0cbeeedee3162400e8829
Sauvikesh <sauviklal@gmail.com>
2024-09-16 14:58:47 -0700

    style: added the h4 to the sage headings.

--- app/(pages)/(index-page)/sage/_components/Headings/Heading.tsx
+++ app/(pages)/(index-page)/sage/_components/Headings/Heading.tsx
@@ -3,12 +3,13 @@ export type HeadingProps = {
   p: string;
 };
 
+// TODO: double check the styling as it doesn't look right
 export default function Heading(props: HeadingProps) {
   const { h1, p } = props;
   return (
     <>
+      <h4>{p}</h4>
       <h2>{h1}</h2>
-      <h3>{p}</h3>
     </>
   );
 }
