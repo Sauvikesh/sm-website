da8f299ffa47e64f9766c7b2632822e69da70f44
Sauvikesh <sauviklal@gmail.com>
2024-09-21 17:50:00 -0700

    refactor: changed project goals to use TextCard component instead of images

--- app/(pages)/(index-page)/sage/_components/Process/Process.tsx
+++ app/(pages)/(index-page)/sage/_components/Process/Process.tsx
@@ -1,34 +1,34 @@
-import Image from 'next/image';
 import Heading from '../Headings/Heading';
+import TextCard, { TextCardProps } from '../../../_components/TextCard';
 
 // TODO: double check how the widths are being used here?? rn the percentage values are for the width of the flex container
 // ^^ is this the best way I can assign the widths? doesn't look simple to change
 export default function Process() {
+  const textCardElements: TextCardProps[] = [
+    {
+      number: '01',
+      header: 'Accessibilty',
+      body: 'Make self-care methods more accessible for users.',
+    },
+    {
+      number: '02',
+      header: 'Accessibilty',
+      body: 'Be mindful of designing for social accessibilty.',
+    },
+    {
+      number: '03',
+      header: 'Accessibilty',
+      body: 'Empowers users to be proactive and better their mental health.',
+    },
+  ];
   return (
-    <div className="flex flex-col pl-[11%] pr-[11%] bg-gray-200 justify-center g-3 h-[60vh]">
+    <div className="flex flex-col pl-[11%] pr-[11%] bg-white justify-center g-3 h-[60vh]">
+      <div className="h-[1px] bg-[#CCC] mb-20"></div>
       <Heading h1="THE PROCESS" p="Project Goals" />
-      <div className="flex">
-        <Image
-          src="/sageImages/process/process1.png"
-          alt="Process Image 1"
-          height={1000}
-          width={1000}
-          className="w-[30%]"
-        ></Image>
-        <Image
-          src="/sageImages/process/process2.png"
-          alt="Process Image 2"
-          height={1000}
-          width={1000}
-          className="w-[30%]"
-        ></Image>
-        <Image
-          src="/sageImages/process/process3.png"
-          alt="Process Image 3"
-          height={1000}
-          width={1000}
-          className="w-[40%]"
-        ></Image>
+      <div className="flex gap-8 pb-20 pt-8">
+        {textCardElements.map((card, index) => (
+          <TextCard {...card} key={index} />
+        ))}
       </div>
     </div>
   );
