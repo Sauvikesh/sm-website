582809f8e96df3bb3ec7ed53eee5d1362f77e82c
Sauvikesh <sauviklal@gmail.com>
2024-09-23 15:49:03 -0700

    style: added extra top and bottom paddings to venue iamge

--- app/(pages)/(index-page)/(case-studies)/hackdavis/_components/Sections/Venue/Venue.tsx
+++ app/(pages)/(index-page)/(case-studies)/hackdavis/_components/Sections/Venue/Venue.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function Venue() {
   return (
-    <section className="w-full">
+    <section className="w-full pt-20 pb-20">
       <Image
         src="hackdavis/venue.svg"
         alt="venue"
