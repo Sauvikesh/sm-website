fd7d87f8c948101a696585921d65019c3aa9c31d
Sauvikesh <sauviklal@gmail.com>
2024-09-22 18:19:03 -0700

    feat: created a reveal animation on scroll wrapper

---
+++ app/(pages)/(index-page)/_components/Reveal/Reveal.tsx
@@ -0,0 +1,26 @@
+'use client';
+import { motion } from 'framer-motion';
+import { ReactNode } from 'react';
+
+type RevealProps = {
+  children: ReactNode;
+};
+
+export const Reveal = ({ children }: RevealProps) => {
+  return (
+    <div>
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        transition={{ duration: 0.5, delay: 0.25 }}
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
+};
