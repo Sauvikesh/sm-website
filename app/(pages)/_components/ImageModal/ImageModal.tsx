d895982d0de16779a5a52d99075ab57976a19d95
Sauvikesh <sauviklal@gmail.com>
2024-09-24 21:34:15 -0700

    fix: linter

--- app/(pages)/_components/ImageModal/ImageModal.tsx
+++ app/(pages)/_components/ImageModal/ImageModal.tsx
@@ -42,37 +42,36 @@ export const ImageWithModal = ({
   );
 };
 
-
 type ImageModalProps = {
-    src: string;
-    alt: string;
-    onClose: () => void;
-  };
-  const ImageModal = ({ src, alt, onClose }: ImageModalProps) => {
-    useEffect(() => {
-      // Lock scroll on the body when the modal opens
-      const originalOverflow = document.body.style.overflow;
-      document.body.style.overflow = 'hidden';
-  
-      // Clean up and restore the original scroll behavior when the modal is closed
-      return () => {
-        document.body.style.overflow = originalOverflow;
-      };
-    }, []);
-    return (
-      <div
-        className="fixed inset-0 z-50 flex justify-center bg-black overflow-scroll"
-        onClick={onClose} // Close the modal if clicked outside the image
-      >
-        <div className="relative">
-          <Image
-            src={src}
-            alt={alt}
-            width={1000}
-            height={1000}
-            className="object-contain w-[100vw] pt-20 pb-20 cursor-zoom-out" // Adjust the size of the image inside the modal
-          />
-        </div>
+  src: string;
+  alt: string;
+  onClose: () => void;
+};
+const ImageModal = ({ src, alt, onClose }: ImageModalProps) => {
+  useEffect(() => {
+    // Lock scroll on the body when the modal opens
+    const originalOverflow = document.body.style.overflow;
+    document.body.style.overflow = 'hidden';
+
+    // Clean up and restore the original scroll behavior when the modal is closed
+    return () => {
+      document.body.style.overflow = originalOverflow;
+    };
+  }, []);
+  return (
+    <div
+      className="fixed inset-0 z-50 flex justify-center bg-black overflow-scroll"
+      onClick={onClose} // Close the modal if clicked outside the image
+    >
+      <div className="relative">
+        <Image
+          src={src}
+          alt={alt}
+          width={1000}
+          height={1000}
+          className="object-contain w-[100vw] pt-20 pb-20 cursor-zoom-out" // Adjust the size of the image inside the modal
+        />
       </div>
-    );
-  };
+    </div>
+  );
+};
