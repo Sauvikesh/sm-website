20565d1ee7512fcff8dc37b081106916c452921f
Sauvikesh <sauviklal@gmail.com>
2024-10-02 20:55:56 -0700

    style: removed unneeded styling for text tags in conclusion in sage

--- app/(pages)/(index-page)/(case-studies)/sage/_components/Sections/Conclusion/Conclusion.tsx
+++ app/(pages)/(index-page)/(case-studies)/sage/_components/Sections/Conclusion/Conclusion.tsx
@@ -4,12 +4,12 @@ import { RevealWrapper } from '@/app/(pages)/(index-page)/_components/Reveal/Rev
 
 export default function Conclustion() {
   return (
-    <section className="flex flex-col">
+    <section className="flex flex-col gap-10">
       <RevealWrapper>
         <div className="flex flex-col gap-4">
           <h4>Reflection</h4>
-          <h3 className="pb-2">Challenges</h3>
-          <p className="pb-10 leading-8">
+          <h3>Challenges</h3>
+          <p>
             Narrowing down the ideas to implement was a challenge considering
             the time constraint of the project. As it was not possible to
             utilize all of the features ideated, the ones that best addressed
@@ -21,8 +21,8 @@ export default function Conclustion() {
         </div>
 
         <div className="flex flex-col gap-4">
-          <h3 className="pb-2">Takeaways</h3>
-          <p className="pb-10 leading-8">
+          <h3>Takeaways</h3>
+          <p>
             The primary takeaway from this project was prioritizing intention
             and users' needs. Behind every design choice, whether it be
             information architecture, brand identity or content design, there
@@ -32,8 +32,8 @@ export default function Conclustion() {
         </div>
 
         <div className="flex flex-col gap-4">
-          <h3 className="pb-2">Next Steps</h3>
-          <p className="pb-10 leading-8">
+          <h3>Next Steps</h3>
+          <p>
             If this project were to move forward, I would love to conduct
             another round of usability testing with hi-fis to further assess any
             additional pain points that may come up and fine-tune the visual
