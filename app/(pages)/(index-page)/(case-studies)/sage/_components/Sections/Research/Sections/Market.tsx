0ab77729af2f1eb3e2621f8b4a4ca6036c831edb
Sauvikesh <sauviklal@gmail.com>
2024-10-05 00:19:53 -0700

    feat: added a competitive analysis component with html tags to replace the full images that were used before

--- app/(pages)/(index-page)/(case-studies)/sage/_components/Sections/Research/Sections/Market.tsx
+++ app/(pages)/(index-page)/(case-studies)/sage/_components/Sections/Research/Sections/Market.tsx
@@ -1,6 +1,23 @@
 import { RevealWrapper } from '@/app/(pages)/(index-page)/_components/Reveal/RevealWrapper';
 import Image from 'next/image';
-
+// TODO: remove images of just headspace and calm squares
+const competitiveAnalysisData: CompetitorAnalysisProps[] = [
+  {
+    logoUrl: '/sageImages/research/calmLogo.png',
+    mission: `"Support every step of
+          their mental health journey."`,
+    advantage:
+      '#1 app for sleep, meditation, and relaxation',
+    disadvantage: 'Goals are pre-set to daily practice, and user can only maintain their streak by completing at least one session per day',
+  },
+  {
+    logoUrl: '/sageImages/research/headspaceLogo.png',
+    mission: `"Provide every person access to lifelong mental health support"`,
+    advantage:
+      'Provides personalized recommendations and information about skills practiced',
+    disadvantage: 'Limited sorting and filtering abilities',
+  },
+];
 export default function Market() {
   return (
     <div className="flex flex-col gap-20">
@@ -15,20 +32,56 @@ export default function Market() {
           </p>
         </div>
         <div className="flex justify-between gap-8 pt-12">
-          <Image
-            src="/sageImages/research/calm.jpg"
-            alt="calm logo"
-            width={1000}
-            height={1000}
-          />
-          <Image
-            src="/sageImages/research/headspace.jpg"
-            alt="headspace logo"
-            width={1000}
-            height={1000}
-          />
+          {competitiveAnalysisData.map((competitor, index) => (
+            <CompetitorAnalysis
+              logoUrl={competitor.logoUrl}
+              mission={competitor.mission}
+              advantage={competitor.advantage}
+              disadvantage={competitor.disadvantage}
+              key={index}
+            />
+          ))}
         </div>
       </RevealWrapper>
+    </div>
+  );
+}
+
+type CompetitorAnalysisProps = {
+  logoUrl: string;
+  mission: string;
+  advantage: string;
+  disadvantage: string;
+};
+function CompetitorAnalysis({
+  logoUrl,
+  mission,
+  advantage,
+  disadvantage,
+}: CompetitorAnalysisProps) {
+  return (
+    <div className="flex flex-col w-1/2 p-8 bg-[#F2F2F2] gap-8">
+      <Image
+        src={logoUrl}
+        alt="competitor app logo"
+        width={1000}
+        height={1000}
+        className="h-[15%] w-auto self-center"
+      />
+      <ul className="flex flex-col gap-6">
+        <li>
+          {' '}
+          <span className="font-bold">Mission:</span> {mission}
+        </li>
+        <li>
+          {' '}
+          <span className="font-bold">Advantage:</span> {advantage}
+        </li>
+        <li>
+          {' '}
+          <span className="font-bold">Disadvantage:</span> {disadvantage}
+        </li>
+      </ul>
     </div>
   );
 }
