96c8efc2d7863a1e006c8adfc8d163694b996f03
Sauvikesh <sauviklal@gmail.com>
2024-11-06 22:14:04 -0800

    feat: turned off image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,8 @@ const nextConfig = {
     domains: [
       'stm-website-private-image-storage.s3.us-east-2.amazonaws.com',
       'd1ejtjbpinwbmz.cloudfront.net',
-    ], // Replace with your actual S3 bucket domain
+    ],
+    unoptimized: true,
   },
 };
 
