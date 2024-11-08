a9e9891d4ceb646e1d9390aa0644046e77c5c58f
Sauvikesh <sauviklal@gmail.com>
2024-11-07 21:58:02 -0800

    style: height of two images are now always the same

--- app/(pages)/(index-page)/(case-studies)/paramount/EPG/_components/Sections/MarketBehavior/MarketBehavior.tsx
+++ app/(pages)/(index-page)/(case-studies)/paramount/EPG/_components/Sections/MarketBehavior/MarketBehavior.tsx
@@ -10,23 +10,23 @@ export default function MarketBehavior(content: contentProps) {
         <HeadingBody h={content.h3?.at(0)} p={content.body?.at(0)} />
 
         <div className="flex justify-between items-end gap-10">
-          <div className="flex w-[747px] flex-col items-center gap-2">
+          <div className="flex w-[747px] flex-col items-center gap-2 self-start">
             <ImageWithModal
               src={content.images?.at(0)?.url}
               alt=""
               width={1000}
               height={1000}
-              className="w-full"
+              className="h-[429px]"
             />
             <h4>{content.h4?.at(0)}</h4>
           </div>
-          <div className="flex w-[213px] flex-col items-center gap-2">
+          <div className="flex w-[213px] flex-col items-center gap-2 self-end">
             <ImageWithModal
               src={content.images?.at(1)?.url}
               alt=""
               width={1000}
               height={1000}
-              className="w-full"
+              className="w-full h-[429px]"
             />
             <h4>{content.h4?.at(1)}</h4>
           </div>
