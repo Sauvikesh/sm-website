5c07ff79a349ae0a6bdf9709e7a98adadcd36754
Sauvikesh <sauviklal@gmail.com>
2024-11-07 20:02:31 -0800

    style: extended width of team image in HD

--- app/(pages)/(index-page)/(case-studies)/hackdavis/_components/Sections/Intro/Intro.tsx
+++ app/(pages)/(index-page)/(case-studies)/hackdavis/_components/Sections/Intro/Intro.tsx
@@ -20,7 +20,7 @@ export default function Intro() {
             </p>
           </div>
 
-          <div className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2 w-[47%]">
             <h3>The Team</h3>
             <Image
               src="/hackdavis/team.svg"
