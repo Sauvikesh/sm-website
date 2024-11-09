27699d2f26c65227fae946a09c18ff0c253a1aa4
Sauvikesh <sauviklal@gmail.com>
2024-11-08 20:24:16 -0800

    style: updated about page picture and repositioned shapes

--- app/(pages)/(index-page)/about/_components/Bio/Bio.tsx
+++ app/(pages)/(index-page)/about/_components/Bio/Bio.tsx
@@ -5,56 +5,67 @@ import LoadingImage from '@/app/(pages)/_components/LoadingImage/LoadingImage';
 export default function Bio() {
   const floatingImages = [
     {
-      src: '/landing/shapes/star.svg',
+      src: '/landing/shapes/4star.svg',
       className:
-        'w-[100px] h-auto absolute top-[550px] left-[11%] hover:rotate-180 transition-transform duration-700 animate-load-rotate-full z-20',
+        'w-[100px] h-auto absolute bottom-[-10%] left-[-2%] hover:rotate-180 transition-transform duration-700 animate-load-rotate-full z-[11]',
     },
     {
       src: '/landing/shapes/cloud.svg',
       className:
-        'w-[100px] h-auto absolute top-[400px] left-[34%] hover:rotate-180 transition-transform duration-700 animate-load-rotate-full z-0',
+        'w-[100px] h-auto absolute top-[40%] right-[-8%] hover:rotate-180 transition-transform duration-700 animate-load-rotate-full z-0',
     },
     {
       src: '/landing/shapes/star2.svg',
       className:
-        'w-[100px] h-auto absolute top-[200px] left-[13%] hover:rotate-180 transition-transform duration-700 animate-load-rotate-full',
-    },
-    {
-      src: '/landing/shapes/4star.svg',
-      className:
-        'w-[100px] h-auto absolute top-[500px] right-[6%] hover:rotate-180 transition-transform duration-700 animate-load-rotate-full',
+        'w-[100px] h-auto absolute top-[-5%] left-[-11%] hover:rotate-180 transition-transform duration-700 animate-load-rotate-full',
     },
     {
       src: '/landing/shapes/spade.svg',
       className:
-        'w-[100px] h-auto absolute top-[50px] right-[-3%] hover:rotate-180 transition-transform duration-700 animate-load-rotate-full',
+        'w-[100px] h-auto absolute top-[-1%] right-[5%] hover:rotate-180 transition-transform duration-700 animate-load-rotate-full',
     },
   ];
   return (
     <RevealWrapper>
-      <section className="flex pl-[15%] pr-[15%] pt-[30vh] h-[80vh] gap-[118px]">
-        {floatingImages.map((image, index) => (
-          <Image
-            src={image.src}
+      <section className="flex px-[13%] pt-[30vh] h-[80vh] gap-[118px]">
+        {floatingImages
+          .slice(3, 4)
+          ?.map((image, index) => (
+            <Image
+              src={image.src}
+              width={1000}
+              height={1000}
+              alt="floating star :)"
+              className={image.className}
+              key={index}
+            />
+          ))}
+
+        <div className="flex relative w-[40%] h-full -mt-14">
+          <LoadingImage
+            src="/about/SamTMah.svg"
+            alt="Sam Photo"
             width={1000}
             height={1000}
-            alt="floating star :)"
-            className={image.className}
-            key={index}
+            className="w-[371px] h-[458px] z-10"
           />
-        ))}
-        <LoadingImage
-          src="/about/SamTMah.png"
-          alt="Sam Photo"
-          width={1000}
-          height={1000}
-          className="w-[315px] h-[329px] z-10"
-        />
 
-        <div className="flex flex-col gap-6">
-          <h3 className="text-4xl tracking-[0.72px]">
-            Nice to meet you, I'm Sam :D
-          </h3>
+          {floatingImages
+            .slice(0, 3)
+            ?.map((image, index) => (
+              <Image
+                src={image.src}
+                width={1000}
+                height={1000}
+                alt="floating star :)"
+                className={image.className}
+                key={index}
+              />
+            ))}
+        </div>
+
+        <div className="flex flex-col gap-6 w-[60%]">
+          <h3 className="text-4xl tracking-[0.72px]">Nice to meet you</h3>
           <p>
             Throughout my childhood, I turned my ideas into reality through
             drawing and building furniture for my dolls. My expansive
