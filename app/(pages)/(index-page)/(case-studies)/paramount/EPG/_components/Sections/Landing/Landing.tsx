0dc1498175b297c60759e7f9f07b0be5d5dd8533
Sauvikesh <sauviklal@gmail.com>
2024-11-08 18:38:13 -0800

    fix: added second ellipse to EPG landing

--- app/(pages)/(index-page)/(case-studies)/paramount/EPG/_components/Sections/Landing/Landing.tsx
+++ app/(pages)/(index-page)/(case-studies)/paramount/EPG/_components/Sections/Landing/Landing.tsx
@@ -1,11 +1,12 @@
 import { contentProps } from '../../../../errorMessaging/page';
 import LoadingImage from '@/app/(pages)/_components/LoadingImage/LoadingImage';
 import { ImageWithModal } from '@/app/(pages)/_components/ImageModal/ImageModal';
+import Image from 'next/image';
 
 export default function Landing(content: contentProps) {
   return (
     <section
-      className="flex flex-col h-[100vh] items-center justify-end gap-10  -mx-[40%]"
+      className="flex flex-col h-[100vh] items-center justify-end gap-10 -mx-[40%] relative"
       style={{
         backgroundImage:
           'radial-gradient(67.21% 51.52% at 67.71% 57.25%, #0064FF 0%, #000 73.44%)',
@@ -17,14 +18,21 @@ export default function Landing(content: contentProps) {
         alt=""
         width={1000}
         height={1000}
-        className="w-[168px]"
+        className="w-[168px] z-10"
       />
-      <div className="flex flex-col w-[670px] items-center gap-4">
+      <Image
+        src="/paramount/blueEllipse.png"
+        alt=""
+        width={1000}
+        height={1000}
+        className="w-[30%] absolute left-0 bottom-0 z-0"
+      />
+      <div className="flex flex-col w-[670px] items-center gap-4 z-10">
         <h2 className="text-center text-white">{content.h2?.at(0)}</h2>
         <h4 className="text-[#ECF1FC]">7 min read</h4>
       </div>
 
-      <div className="relative">
+      <div className="relative z-10">
         <LoadingImage
           src={content.images?.at(0)?.url}
           alt=""
