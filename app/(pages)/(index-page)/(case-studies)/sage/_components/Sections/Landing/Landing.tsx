fe33d02029a79ade9e62bed1d4c0a840ab2f55a0
Sauvikesh <sauviklal@gmail.com>
2024-11-14 21:33:59 -0800

    feat: added new sage hero images

--- app/(pages)/(index-page)/(case-studies)/sage/_components/Sections/Landing/Landing.tsx
+++ app/(pages)/(index-page)/(case-studies)/sage/_components/Sections/Landing/Landing.tsx
@@ -1,6 +1,6 @@
 'use client';
-import { Providers } from '@/app/_providers';
-import { Parallax } from 'react-scroll-parallax';
+// import { Providers } from '@/app/_providers';
+// import { Parallax } from 'react-scroll-parallax';
 import { useState } from 'react';
 import Loader from '@/app/(pages)/_components/Loader/Loader';
 import Image from 'next/image';
@@ -30,59 +30,66 @@ export default function Landing() {
       src: '/sageImages/hero/appImage3.png',
       alt: 'app image 3',
     },
+    {
+      speed: 19,
+      src: '/sageImages/hero/appImage4.png',
+      alt: 'app image 3',
+    },
   ];
 
   return (
-    <Providers>
-      <header
-        className="flex flex-col gap-6 h-[100vh] items-center justify-end overflow-clip bg-[#E2EDEB] mx-[-40%]"
-        style={{
-          backgroundImage: "url('/sageImages/hero/sageHeroBG.png')",
-          backgroundSize: 'cover',
-        }}
-      >
-        <ImageWithModal
-          src="/sageImages/sageLogo.png"
-          alt="Sage Logo"
-          width={1000}
-          height={1000}
-          className="h-[40px] w-[82px]"
-        />
-        <h2 className="w-[668px] text-center text-[#1B454F]">
-          Equipping individuals to navigate life's stressors
-        </h2>
-        <p className="text-base text-[#1B454F]">6 min read</p>
-        <div className="flex -mb-[290px] -translate-y-10">
-          {parallaxImages.map((image, index) => {
-            if (index === 0) {
-              return (
-                <Parallax speed={image.speed} key={index}>
-                  <LoadingImage
-                    src={image.src}
-                    alt={image.alt}
-                    width={1000}
-                    height={1000}
-                    className="w-[30vw]"
-                  ></LoadingImage>
-                </Parallax>
-              );
-            } else {
-              return (
-                <Parallax speed={image.speed} key={index}>
-                  <Image
-                    src={image.src}
-                    alt={image.alt}
-                    width={1000}
-                    height={1000}
-                    className="w-[30vw]"
-                  ></Image>
-                </Parallax>
-              );
-            }
-          })}
-        </div>
-      </header>
-    </Providers>
+    // <Providers>
+    <header
+      className="flex flex-col gap-6 h-[100vh] items-center justify-end overflow-clip bg-[#E2EDEB] mx-[-40%]"
+      style={{
+        backgroundImage: "url('/sageImages/hero/sageHeroBG.png')",
+        backgroundSize: 'cover',
+      }}
+    >
+      <ImageWithModal
+        src="/sageImages/sageLogo.png"
+        alt="Sage Logo"
+        width={1000}
+        height={1000}
+        className="h-[40px] w-[82px]"
+      />
+      <h2 className="w-[668px] text-center text-[#1B454F]">
+        Equipping individuals to navigate life's stressors
+      </h2>
+      <p className="text-base text-[#1B454F]">6 min read</p>
+      <div className="flex gap-[60px] -mb-10">
+        {parallaxImages.map((image, index) => {
+          if (index === 0) {
+            return (
+              // <Parallax speed={image.speed} key={index}>
+              <LoadingImage
+                src={image.src}
+                alt={image.alt}
+                width={1000}
+                height={1000}
+                className="w-[17vw]"
+                key={index}
+              ></LoadingImage>
+              // </Parallax> */
+            );
+          } else {
+            return (
+              // <Parallax speed={image.speed} key={index}>
+              <Image
+                src={image.src}
+                alt={image.alt}
+                width={1000}
+                height={1000}
+                className="w-[17vw]"
+                key={index}
+              ></Image>
+              // </Parallax> */
+            );
+          }
+        })}
+      </div>
+    </header>
+    // </Providers>
   );
 }
 
